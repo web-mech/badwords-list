eb2e0ee1873452731d6bfb8ae881206c4edffef5
Christophe ROGER <darwiin@users.noreply.github.com>
2022-08-02 16:31:34 +1100

    Update dependencies

--- test/bad-words-integration.js
+++ test/bad-words-integration.js
@@ -5,15 +5,15 @@ describe('bad-words filter', function(){
   it("should return 'xxxxxx de xxxxx' for 'bordel de merde' input", function(){
     var list = require('../dist/index');
     var filter = new Filter({ placeHolder: 'x', emptyList: true}); 
-    filter.addWords(list.array);
+    filter.addWords(...list.array);
 
     filter.clean('bordel de merde').should.be.equal('xxxxxx de xxxxx');
   });
 
   it("should return true for 'conne' input", function(){
     var list = require('../dist/index');
-    var filter = new Filter({ placeHolder: 'x', emptyList: true}); 
-    filter.addWords(list.array);
+    var filter = new Filter({emptyList: true}); 
+    filter.addWords(...list.array);
 
     filter.isProfane('conne').should.be.true;
   });
@@ -21,7 +21,7 @@ describe('bad-words filter', function(){
   it("should return false for 'bonjour' input", function(){
     var list = require('../dist/index');
     var filter = new Filter({ emptyList: true}); 
-    filter.addWords(list.array);
+    filter.addWords(...list.array);
 
     filter.isProfane('bonjour').should.be.false;
   });
