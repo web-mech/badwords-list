0cf07465ac97627a6e631ffc726bedde375cd1eb
web-mech <webmech@gmail.com>
2024-08-11 00:18:37 -0400

    fix(*): use explicit named exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
-export * from './array.js'
-export * from './object.js'
-export * from './regexp.js'
+export { array } from './array.js'
+export { object } from './object.js'
+export { regexp } from './regexp.js'
